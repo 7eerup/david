e1e9f56699e1fa4fd50db433915e96d13511418c
7eerup <7eerup@gmail.com>
2025-08-05 19:42:18 +0900

    Feat: 컴퓨터 이름 표시 기능 추가
    Flask 앱에서 debug 모드일 때 컴퓨터(인스턴스) 이름을 화면에 출력하도록 기능 추가
    socket 모듈을 사용하여 hostname을 가져오며 index.html에 전달
    
    - socket 모듈 import
    - home() 함수에서 hostname 설정 로직 추가
    - render_template 호출 시 computername 인자 추가
    - index.html에 {{ computername }} 표시
    - __debug__ 플래그 확인을 위한 print 추가
    
    Resolves: #2

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    return render_template('index.html', computername=hostname)
 
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
 
 if __name__ == "__main__":
+    print("__debug__ is", __debug__)
     app.run(host='0.0.0.0', port=5000, debug=True)
